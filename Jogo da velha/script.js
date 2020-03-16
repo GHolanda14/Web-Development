4704778c40205c508e7b59f721863586d7d83aad
GHolanda14 <GHolanda14@users.noreply.github.com>
2020-03-16 19:30:55 -0300

    Draw added

--- Jogo da velha/script.js
+++ Jogo da velha/script.js
@@ -19,14 +19,13 @@ function jogada(id){
 			document.getElementById(id).style.backgroundColor = "blue";
 			jogador = 0;		
 		}
-		campeaoVertical();
-		campeaoHorizontal();
-		campeaoDiagonal();
-		empate();
+		if(!vencedor()){
+			empate();
+		}
 	}
 	else{
 		window.alert("Nao pode");
-	}
+	}	
 }
 
 function campeaoHorizontal(){
@@ -34,46 +33,54 @@ function campeaoHorizontal(){
 	v2 = document.getElementById("meiTop").style.backgroundColor;
 	v3 = document.getElementById("dirTop").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
 
 	v1 = document.getElementById("esqMid").style.backgroundColor;
 	v2 = document.getElementById("meiMid").style.backgroundColor;
 	v3 = document.getElementById("dirMid").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
 
 	v1 = document.getElementById("esqBot").style.backgroundColor;
 	v2 = document.getElementById("meiBot").style.backgroundColor;
 	v3 = document.getElementById("dirBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
-
-}
+	return false;
+}/* Se for o veremlho ainda pode ganhar mesmo que ainda haja só uma celula sobrando
+	Bug do vencedor e velha ao mesmo tempo quando sobra so um espaço*/
 
 function campeaoVertical(){
 	v1 = document.getElementById("esqTop").style.backgroundColor;
 	v2 = document.getElementById("esqMid").style.backgroundColor;
 	v3 = document.getElementById("esqBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
 
 	v1 = document.getElementById("meiTop").style.backgroundColor;
 	v2 = document.getElementById("meiMid").style.backgroundColor;
 	v3 = document.getElementById("meiBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
 
 	v1 = document.getElementById("dirTop").style.backgroundColor;
 	v2 = document.getElementById("dirMid").style.backgroundColor;
 	v3 = document.getElementById("dirBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
+	return false;
 }
 
 function campeaoDiagonal(){
@@ -81,24 +88,49 @@ function campeaoDiagonal(){
 	v2 = document.getElementById("meiMid").style.backgroundColor;
 	v3 = document.getElementById("dirBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
+		anuncio(v1);
+		return true;
 	}
 
 	v1 = document.getElementById("dirTop").style.backgroundColor;
 	v2 = document.getElementById("meiMid").style.backgroundColor;
 	v3 = document.getElementById("esqBot").style.backgroundColor;
 	if(v1 == v2 && v2 == v3){
-		ganhador(v1);
-	}	
+		anuncio(v1);
+		return true;
+	}
+	return false;
 }
 
-function ganhador(cor){
+function empate(){
+	var contador = 0;
+	var tabela = document.getElementById("tabela");
+	var celula = tabela.getElementsByTagName("td");
+	for(var i = 0; i < celula.length;i++){
+		if(celula[i].style.backgroundColor == ""){
+			contador++;
+		}
+	}
+	if(contador <= 1 && jogador != 0){
+		setTimeout( function(){alert("Deu velha!");confirmacao();},10);
+	}
+}
+
+function anuncio(cor){
 	if(cor == "red"){
 		setTimeout( function(){alert("Jogador N 1 ganhou!");confirmacao();},10);
 	}
 	else if(cor == "blue"){
 		setTimeout( function(){alert("Jogador N 2 ganhou!");confirmacao();},10);
 	}
+	
+}
+
+function vencedor(){
+	if(campeaoVertical() || campeaoHorizontal() || campeaoDiagonal()){
+		return true;
+	} 
+	return false;
 }
 
 function confirmacao(){
@@ -107,12 +139,5 @@ function confirmacao(){
 	}
 	else{
 		document.getElementById("tabela").style.pointerEvents = "none";
-	}
-}
-
-function empate(){
-	var tabela = document.getElementById("tabela");
-	for (var i = 0,cell; cell = tabela.cells[i];i++){
-		validar(cell);
 	}
 }
